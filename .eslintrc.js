3883e3e37d7b9d97773eb3df09f526161cb73e38
Austin Langdon <alangdon93@gmail.com>
2023-06-10 15:24:52 +0700

    minimal eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
-  extends: ['eslint:recommended', 'next', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'unicorn'],
+  extends: ['next', 'prettier'],
+  plugins: ['unicorn'],
   rules: {
     'no-unused-vars': 'off',
     'prefer-const': 'error',
     'react-hooks/exhaustive-deps': 'error',
     'react/no-unescaped-entities': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
     'unicorn/filename-case': [
       'error',
       {
         case: 'kebabCase'
       }
     ]
-  },
-  root: true
+  }
 };
