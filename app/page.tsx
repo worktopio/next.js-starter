6040f86ae94b9b3f084034f65c3365b1cfe7963b
Austin Langdon <alangdon93@gmail.com>
2023-06-17 16:08:53 +0700

    sitemap

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import '@worktopio/sitemap/lib/types';
+
+export const metadata: Metadata = {
+  title: 'Home',
+  datePublished: '1/1/2023',
+  breadcrumbs: [{ name: 'Home', href: '/' }]
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
